b76556c091636eae7fa27e1efad3bbc13d546041
agnes0304 <jiwoochoi0304@gmail.com>
2023-05-01 17:18:54 +0900

    DOM practice: CRUD ongoing

--- basic-07-dom/assets/scripts/app.js
+++ basic-07-dom/assets/scripts/app.js
@@ -5,36 +5,113 @@ const backdrop = document.querySelector('#backdrop');
 const cancelModalBtn = addMovieModal.querySelector('.btn--passive'); //addMovieModal이 있으니까 전체 document로 훑지 않아도 됨.
 const addModalbtn = cancelModalBtn.nextElementSibling;
 const inputs = addMovieModal.querySelectorAll('input');
+const entrytextSection = document.getElementById('entry-text');
 
+const movies = [];
+
+const updateUI = () => {
+    if (movies.length === 0){
+        entrytextSection.style.display = 'block';
+    } else {
+        entrytextSection.style.display = 'none';
+    }
+};
+
+const deleteMovieHandler = (movieId) => {
+    let movieIndex = 0;
+    for (const movie of movies){
+        if (movie.id === movieId){
+            break;
+        }
+        movieIndex++;
+    }
+    movies.splice(movieIndex, 1);
+    const listRoot = document.getElementById('movie-list');
+    listRoot.children[movieIndex].remove();
+};
+
+// 006 화면에 영화 항목 렌더링
+const renderNewMovie = (id, title, imageUrl, rating) => {
+    const newMovieEl = document.createElement('li');
+    newMovieEl.className = 'movie-element'; // CSS파일에 가면 있음. 
+    // html 내용이 바뀌는게 아니기에 하드코딩함
+    newMovieEl.innerHTML = `
+    <div class="movie-element__image">
+        <img src="${imageUrl}" alt="${title}">
+    </div>
+    <div class="movie-element__info">
+        <h2>${title}</h2>
+        <p>${rating}/5 stars</p>
+    </div>
+    `;
+    
+    // 007 영화 항목 삭제
+    newMovieEl.addEventListener('click', deleteMovieHandler.bind(null, id));
+    const listRoot = document.getElementById('movie-list');
+    listRoot.append(newMovieEl);
+};
 
 // 나중에 다른 곳에서도 쓸 수 있게 만드려고
 const toggleBackdrop = () => {
     backdrop.classList.toggle('visible');
-}
+};
 
 const toggleModal = () => {
     addMovieModal.classList.toggle('visible'); // toggle()메서드는 내가 원하는 클래스가 있으면 없애고, 없으면 추가하는 애임
     toggleBackdrop();
-}
+};
 
-const backdropClickHandler = () =>{
-    toggleModal();
-}
+// 005 인풋 치워주기
+const clearInput = () => {
+    for (const input of inputs) {
+        input.value = '';
+    }
+};
 
 const cancelModalHandler = () => {
     toggleModal();  
-}
+    clearInput();
+};
 
 const addMovieHandler = () => {
     const titleVal = inputs[0].value;
     const imgUrlVal = inputs[1].value;
     const ratingVal = inputs[2].value;
     
-    // trim()은 불필요한 공백을 제거해주는 것.
-    if (titleVal.trim() ===''||imgUrlVal.trim()===''|| ratingVal.trim()===''||ratingVal < 1|| ratingVal > 5) {
-        alert("Please enter valid value.") // 커스텀 모달을 사용해도 되긴 함. 
-        return
+    if (
+        titleVal.trim() ==='' || 
+        imgUrlVal.trim() ==='' ||
+        ratingVal.trim() ==='' || 
+        +ratingVal < 1 || 
+        +ratingVal > 5
+        ) {
+        alert("Please enter valid value.");
+        return;
     }
+
+    // 004 영화 생성 및 입력
+    const newMovie = {
+        id: Math.random().toString(), // 원래는 이렇게 하면 안돼. 랜덤한 수를 생성한다고 해서 중복이 안된다는 법도 없고, 일단 고유한 값을 가지게 하는 방법은 아님. 
+        title: titleVal,
+        image: imgUrlVal,
+        rating: ratingVal
+    };
+
+    movies.push(newMovie);
+    toggleModal();
+    clearInput();
+    renderNewMovie(
+        newMovie.id, 
+        newMovie.title, 
+        newMovie.image, 
+        newMovie.rating
+        );
+    updateUI();
+}
+
+const backdropClickHandler = () =>{
+    toggleModal();
+};
 
 // 001 add modal 버튼 클릭 -> id=add-modal, backdrop에 .visible 추가
 startAddMovieBtn.addEventListener('click', toggleModal);
