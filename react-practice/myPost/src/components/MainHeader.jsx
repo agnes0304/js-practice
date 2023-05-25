c7e80e7ac90431fd32f2160b61920d311f273f80
agnes0304 <jiwoochoi0304@gmail.com>
2023-05-25 18:28:25 +0900

    myPost: MainHeader ongoing

--- react-practice/myPost/src/components/MainHeader.jsx
+++ react-practice/myPost/src/components/MainHeader.jsx
@@ -1,4 +1,5 @@
 import { MdMessage, MdPostAdd } from "react-icons/md"; // 아이콘 쓰기 위함. npm install 해줘야 사용할 수 있음.
+import classes from './MainHeader.module.css';
 
 function MainHeader() {
   return (
@@ -8,7 +9,7 @@ function MainHeader() {
         Message Poster
       </h1>
       <p>
-        <button className={classes.button} onClick={onCreatePost}>
+        <button className={classes.button}>
           <MdPostAdd size={18} />
           New Post
         </button>
