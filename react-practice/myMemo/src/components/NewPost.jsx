33c5361c381f349ec8d1472c85af9ba43ddd1f6d
agnes0304 <jiwoochoi0304@gmail.com>
2023-05-30 18:39:54 +0900

    myMemo: NewPost.jsx

--- react-practice/myMemo/src/components/NewPost.jsx
+++ react-practice/myMemo/src/components/NewPost.jsx
@@ -1,6 +1,23 @@
+import classes from './NewPost.module.css'
 function NewPost() {
-    return <>
+  return (
+    <>
+      <form className={classes.form}>
+        <p>
+          <label htmlFor="name">Leave your name</label>
+          <input id="name" type="text" placeholder='Name' />
+        </p>
+        <p>
+          <label htmlFor="body">Leave your memo</label>
+          <textarea id="body" rows={3} placeholder="Memo" />
+        </p>
+        <p className={classes.actions}>
+          <button type="submit">Write</button>
+          <button type="button">Cancel</button>
+        </p>
+      </form>
     </>
+  );
 }
 
-export default NewPost
+export default NewPost;
