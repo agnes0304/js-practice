801dceaa44b60b7c19287590b0da09333b55a754
agnes0304 <jiwoochoi0304@gmail.com>
2023-05-30 21:06:41 +0900

    myMemo: Modal 작업

--- react-practice/myMemo/src/components/Modal.jsx
+++ react-practice/myMemo/src/components/Modal.jsx
@@ -2,8 +2,8 @@ import classes from './Modal.module.css'
 
 function Modal(props) {
     return<>
+        <div className={classes.backdrop} onClick={props.onClose}></div>
         <div open className={classes.modal}>{props.children}</div>
-        <div className={classes.backdrop} onClick={hideModal}></div>
     </>
 }
 
--- react-practice/myMemo/src/components/PostList.jsx
+++ react-practice/myMemo/src/components/PostList.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
-import classes from './Postlist.module.css'
+import classes from "./Postlist.module.css";
+import Modal from "./Modal";
 
 function PostList() {
-  const [ enteredName, setName ] = useState('');
-  const [ enteredBody, setBody ] = useState('');
+  const [enteredName, setName] = useState("");
+  const [enteredBody, setBody] = useState("");
+  const [isView, setView] = useState(true);
 
   function onNameHandler(event) {
     setName(event.target.value);
@@ -15,9 +17,18 @@ function PostList() {
     setBody(event.target.value);
   }
 
+  function hideModal() {
+    setView(false);
+  }
+
   return (
     <>
-      <NewPost nameHandler={onNameHandler} bodyHandler={onBodyHandler} />
+      {isView ? (
+        <Modal onClose={hideModal}>
+          <NewPost nameHandler={onNameHandler} bodyHandler={onBodyHandler} />
+        </Modal>
+      ) : null}
+
       <ul className={classes.posts}>
         <Post name={enteredName} body={enteredBody} />
       </ul>
