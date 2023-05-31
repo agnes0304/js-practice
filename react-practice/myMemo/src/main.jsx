eb7132c081b60fa9a4fe107bd9f2b5e4e336dea8
agnes0304 <jiwoochoi0304@gmail.com>
2023-05-31 11:55:37 +0900

    myMemo: route 작업

--- react-practice/myMemo/src/main.jsx
+++ react-practice/myMemo/src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import NewPost from './components/NewPost.jsx';
+import RootLayout from './routes/rootLayout.jsx';
+
+const router = createBrowserRouter([
+  { path: '/', element: <RootLayout />, children: [
+    { path: '/', element: <App />},
+    { path: '/newpost', element: <NewPost />},
+    { path: '/detail', element: <Detail />},
+  ]},
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
   </React.StrictMode>,
 )
---
+++ react-practice/myMemo/src/routes/RootLayout.jsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router-dom"
+import Header from "../components/Header"
+
+function RootLayout() {
+    return<>
+        <Header />
+        <Outlet />
+    </>
+}
+export default RootLayout
