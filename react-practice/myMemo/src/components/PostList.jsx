735f21ba2c4a74f7c265b7d8c79dbf18005e07f9
agnes0304 <jiwoochoi0304@gmail.com>
2023-05-31 12:54:05 +0900

    re start

--- react-practice/myMemo/src/components/PostList.jsx
+++ react-practice/myMemo/src/components/PostList.jsx
@@ -7,29 +7,28 @@ import Modal from "./Modal";
 import Detail from "./Detail";
 
 function PostList(props) {
-  
   const [posts, setPost] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-	
-	useEffect(()=>{
-		async function fetchPosts() {
-			setIsFetching(true);
-			const response = await fetch('http://localhost:3000/');
-			const resData = await response.json();
-			setPosts(resData.posts);
-			setIsFetching(false);
-		}
-		fetchPosts();
-	}, []);
+  // const [isFetching, setIsFetching] = useState(false);
+
+  // useEffect(()=>{
+  // 	async function fetchPosts() {
+  // 		setIsFetching(true);
+  // 		const response = await fetch('http://localhost:3000/');
+  // 		const resData = await response.json();
+  // 		setPosts(resData.posts);
+  // 		setIsFetching(false);
+  // 	}
+  // 	fetchPosts();
+  // }, []);
 
   function addPostHandler(data) {
-    fetch('http://localhost:3000/newpost', {
-			method: 'POST',
-			body: JSON.stringify(postData),
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		})
+    // fetch('http://localhost:3000/newpost', {
+    // 	method: 'POST',
+    // 	body: JSON.stringify(postData),
+    // 	headers: {
+    // 		'Content-Type': 'application/json'
+    // 	}
+    // })
     setPost([data, ...posts]);
   }
 
@@ -41,7 +40,8 @@ function PostList(props) {
         </Modal>
       ) : null}
 
-      {!isFetching && posts.length > 0 && (
+      {/* {!isFetching && posts.length > 0 && ( */}
+      {posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((data) => (
             <Post key={data.body.length} name={data.name} body={data.body} />
@@ -49,14 +49,20 @@ function PostList(props) {
         </ul>
       )}
 
-      {!isFetching && posts.length === 0 && (
-				<div style={{textAlign: 'center', color: 'white'}}>
-					<h1>There are no posts yet.</h1>
-					<p>Leave your message here!</p>
-				</div>
-			)}
-      { isFetching && (<div style={{textAlign: 'center', color: 'white'}}><p>Loading Posts Now...</p></div>)}
-      
+      {/* {!isFetching && posts.length === 0 && ( */}
+      { posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h1>There are no posts yet.</h1>
+          <p>Leave your message here!</p>
+        </div>
+      )}
+
+      {/* {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading Posts Now...</p>
+        </div>
+      )} */}
+
       <Detail />
     </>
   );
--- react-practice/myMemo/src/main.jsx
+++ react-practice/myMemo/src/main.jsx
@@ -6,17 +6,17 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import NewPost from './components/NewPost.jsx';
 import RootLayout from './routes/RootLayout.jsx';
 
-const router = createBrowserRouter([
-  { path: '/', element: <RootLayout />, children: [
-    { path: '/', element: <App />},
-    { path: '/newpost', element: <NewPost />},
-    { path: '/detail', element: <Detail />},
-  ]},
-])
+// const router = createBrowserRouter([
+//   { path: '/', element: <RootLayout />, children: [
+//     { path: '/', element: <App />},
+//     { path: '/newpost', element: <NewPost />},
+//     { path: '/detail', element: <Detail />},
+//   ]},
+// ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
     <App />
   </React.StrictMode>,
 )
