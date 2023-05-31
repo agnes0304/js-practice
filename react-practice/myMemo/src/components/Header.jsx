c2e8bd577d8dc9dc3b4e13b06bd10d387d1e4004
agnes0304 <jiwoochoi0304@gmail.com>
2023-05-31 15:53:06 +0900

    myMemo: design change

--- react-practice/myMemo/src/components/Header.jsx
+++ react-practice/myMemo/src/components/Header.jsx
@@ -2,12 +2,16 @@ import classes from "./Header.module.css";
 
 function Header(props) {
   return (
+    <>
       <header className={classes.header}>
         <h1 className={classes.title}>My Memo</h1>
         <button className={classes.button} onClick={props.onCreatePost}>
-          <img src="src/assets/newpost.png" />Create
+          <img src="src/assets/newpost.png" />
+          Create
         </button>
       </header>
+      <hr />
+    </>
   );
 }
 
