0e33b5caa0622f69a5ed9ca57069b2cbc2d78bde
agnes0304 <jiwoochoi0304@gmail.com>
2023-06-01 13:41:32 +0900

    myMemo: routing ongoing

--- react-practice/myMemo-server/server.js
+++ react-practice/myMemo-server/server.js
@@ -1,18 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
-const routes = require('routes.js');
 
 const app =express();
+app.use(cors());
 
 app.use(bodyParser.json());
 
-app.use((req, res, next)=>{
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-	next();
-})
 
-app.use(routes);
 
-app.listen(3000);
+
+app.listen(5000);
